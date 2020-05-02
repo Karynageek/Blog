b1e3964b16e9928b1be23128cb59ff15e6425adc
Karyna Chelpan <cheplan15@gmail.com>
2020-05-02 20:14:29 +0300

    create project

---
+++ src/main/java/com/myexample/blog/controllers/MessageController.java
@@ -0,0 +1,47 @@
+package com.myexample.blog.controllers;
+
+import com.myexample.blog.models.Message;
+import com.myexample.blog.models.Post;
+import com.myexample.blog.repo.MessageRepo;
+import com.myexample.blog.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class MessageController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/message")
+    public  String messageViews(Model model){
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+        return "message";
+    }
+    @PostMapping("/message")
+    public  String messageSendAll(@RequestParam String tag, @RequestParam String text ){
+        Message message = new Message(tag, text);
+        messageRepo.save(message);
+        return "redirect:/blog";
+    }
+    @PostMapping("/blog/{id}")
+    public  String messageSend(@RequestParam String tag, @RequestParam String text ){
+        Message message = new Message(tag, text);
+        messageRepo.save(message);
+        return "redirect:/blog/{id}";
+    }
+    @PostMapping("/blog/{id}/filter")
+    public  String filter(@RequestParam String filter, Map<String, Object> model ){
+        List<Message> messages = messageRepo.findByTag(filter);
+        model.put("messages", messages);
+        return "blog-details";
+    }
+}
---
+++ src/main/java/com/myexample/blog/models/Message.java
@@ -0,0 +1,57 @@
+package com.myexample.blog.models;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String text;
+    private String tag;
+    private User author;
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Message() {
+    }
+    public Message(String tag, String text ) {
+        this.tag=tag;
+        this.text=text;
+    }
+}
